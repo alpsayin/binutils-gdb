610ec50bbb4e553d86f791f2f0cefcb221ba8740
Mahesh Bodapati <mbodapat@xilinx.com>
2021-11-09 16:19:17 +0530

    Added m64 abi for 64 bit target descriptions. set m64 abi for 64 bit elf.
    Signed-off-by: Alp Sayin <alpsayin@gmail.com>

--- gdb/microblaze-tdep.h
+++ gdb/microblaze-tdep.h
@@ -19,9 +19,17 @@
 
 #ifndef MICROBLAZE_TDEP_H
 #define MICROBLAZE_TDEP_H 1
-
+#include "objfiles.h"
 #include "gdbarch.h"
 
+struct gdbarch;
+enum microblaze_abi
+  {
+    MICROBLAZE_ABI_AUTO = 0,
+    MICROBLAZE_ABI_M64,
+ };
+
+enum microblaze_abi microblaze_abi (struct gdbarch *gdbarch);
 /* Microblaze architecture-specific information.  */
 struct microblaze_gdbarch_tdep : gdbarch_tdep
 {
@@ -39,11 +47,14 @@ struct gdbarch_tdep
 {
   int dummy;		// declare something.
 
+  enum microblaze_abi microblaze_abi;
+  enum microblaze_abi found_abi;
   /* Register sets.  */
   struct regset *gregset;
   size_t sizeof_gregset;
   struct regset *fpregset;
   size_t sizeof_fpregset;
+  int register_size;
 };
 
 /* Register numbers.  */
